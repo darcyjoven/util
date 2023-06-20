d5e9fc9e2d041682b481917c47da23ebd485a6d6
darcy <darcy_joven@live.com>
2023-06-20 12:31:37 +0800

    ✨ feat(log): 时区设置为shanghai

--- zapper/entry.go
+++ zapper/entry.go
@@ -2,10 +2,18 @@ package zapper
 
 import (
 	"encoding/json"
+	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
+
+type constantClock time.Time
+
+func (c constantClock) Now() time.Time { return time.Time(c) }
+func (c constantClock) NewTicker(d time.Duration) *time.Ticker {
+	return &time.Ticker{}
+}
 
 func Exec(file string) (logger *zap.Logger, err error) {
 	rawJSON := []byte(`{
@@ -22,7 +30,12 @@ func Exec(file string) (logger *zap.Logger, err error) {
 	cfg.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("06-01-02 15:04:05.00")
 	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 
-	logger = zap.Must(cfg.Build())
+	local, _ := time.LoadLocation("Asia/Shanghai")
+	date := time.Now().In(local)
+	clock := constantClock(date)
+	logger = zap.Must(cfg.Build(
+		zap.WithClock(clock),
+	))
 	// defer logger.Sync()
 	return logger, nil
 }
