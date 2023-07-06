910537d67a5d0ecb656848b3af8dc0f112206b6f
darcy <darcy_joven@live.com>
2023-07-06 13:33:55 +0800

    ✨ feat(zap/clock): 去掉clock，改为时区显示

--- zapper/entry.go
+++ zapper/entry.go
@@ -3,18 +3,10 @@ package zapper
 import (
 	"encoding/json"
 	"os"
-	"time"
 
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
-
-type constantClock time.Time
-
-func (c constantClock) Now() time.Time { return time.Time(c) }
-func (c constantClock) NewTicker(d time.Duration) *time.Ticker {
-	return &time.Ticker{}
-}
 
 func Exec(file string) (logger *zap.Logger, err error) {
 	rawJSON := []byte(`{
@@ -28,14 +20,10 @@ func Exec(file string) (logger *zap.Logger, err error) {
 	}
 	cfg.OutputPaths = append(cfg.OutputPaths, "stdout", file)
 	cfg.EncoderConfig = zap.NewProductionEncoderConfig()
-	cfg.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("06-01-02 15:04:05.00")
+	cfg.EncoderConfig.EncodeTime = zapcore.TimeEncoderOfLayout("-7:00 06-01-02 15:04:05.00")
 	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
 
-	local, _ := time.LoadLocation("Asia/Shanghai")
-	date := time.Now().In(local)
-	clock := constantClock(date)
 	logger = zap.Must(cfg.Build(
-		zap.WithClock(clock),
 		zap.Fields(zap.Int("pid", os.Getpid())),
 	))
 	// defer logger.Sync()
